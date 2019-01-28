ce18814183df88c58d2ef7cc840e45782844a55d
Shixiang Ren <doggy-ren@hotmail.com>
2019-01-28 18:57:37 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,16 @@
-function basicTeenager(age) {
-
+function basicTeenager(age){
+  if (age>=13 && age<=19) {
+    return 'You are a teenager!'
+  }
+  else {}
 }
+
 
 function teenager(age) {
-
-}
-
-function ageChecker(age) {
-
-}
-
-function ternaryTeenager(age) {
-
-}
-
-function switchAge(age) {
-
+  if(age>=13 && age<=19){
+    return "You are a teenager!"
+  }
+  else{
+    return "You are not a teenager"
+  }
 }
